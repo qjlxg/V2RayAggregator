c43e034bb1abdf4d71b41bfab4fcb4fa97f36622
qjlxg <qjlxg@163.com>
2024-10-31 14:54:57 +0800

    Update speedtest2_yaml.sh

--- utils/speedtest/speedtest2_yaml.sh
+++ utils/speedtest/speedtest2_yaml.sh
@@ -4,4 +4,4 @@ gzip -d lite-linux-amd64.gz
 wget -O lite_config.json https://github.com/qjlxg/V2RayAggregator/raw/refs/heads/master/utils/speedtest/lite_config_yaml.json
 #运行 LiteSpeedTest
 chmod +x ./lite-linux-amd64
-sudo nohup ./lite-linux-amd64 --config ./lite_config.json --test https://raw.githubusercontent.com/mahdibland/SSAggregator/master/sub/sub_merge_yaml.yml|https://github.com/qjlxg/V2RayAggregator/raw/refs/heads/master/sub/sub_merge_yaml.yml > speedtest.log 2>&1 &
+sudo nohup ./lite-linux-amd64 --config ./lite_config.json --test https://raw.githubusercontent.com/mahdibland/SSAggregator/master/sub/sub_merge_yaml.yml|https://github.com/qjlxg/V2RayAggregator/raw/refs/heads/master/sub/sub_merge_yaml.yml|https://raw.githubusercontent.com/yuewei5215/Proxies-1/02b2a3d9a29697df59951aa44cf604f55b62a6a4/sub/sub_merge_yaml.yml|https://raw.githubusercontent.com/flappyD/V2RayAggregator/master/sub/sub_merge_yaml.yml|https://raw.githubusercontent.com/mfbpn/nodelist/main/sub/sub_merge_yaml.yml|https://raw.githubusercontent.com/Airuop/cross/master/sub/sub_merge_yaml.yml > speedtest.log 2>&1 &
